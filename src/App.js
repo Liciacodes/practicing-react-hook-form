fea745de647a25e4f51aa02343a6b7987429961b
Felicia Udosen <udosenfelicia@gmail.com>
2023-02-28 20:29:17 +0100

    practicing react form hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useForm } from "react-hook-form";
+import "./App.css";
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      age: "",
+    },
+  });
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Please Register</h1>
+      <div className="form-div">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            {...register("firstName", {
+              required: "This is required",
+              minLength: {
+                value: 4,
+                message: "Min Length is 4",
+              },
+            })}
+            placeholder="First Name"
+          />
+
+          <p>{errors.firstName?.message}</p>
+          <input
+            {...register("lastName", {
+              required: "This is required",
+              minLength: {
+                value: 4,
+                message: "Min Length is 4",
+              },
+            })}
+            placeholder="Last Name"
+          />
+
+          <input type="number" {...register("age", { valueAsNumber: true })} />
+          <p>{errors.lastName?.message}</p>
+          <input type="submit" className="submit" />
+        </form>
+      </div>
     </div>
   );
 }
